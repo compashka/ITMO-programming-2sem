897527a61e3269c5b18fc9c1c84b0e45b7038e23
Pavel <pavpleshanov@gmail.com>
2022-03-22 14:25:18 +0300

    hw6.1

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -37,13 +37,33 @@ const negate = expr => {
     return (x, y, z) => -(expr(x, y, z));
 }
 
+const iff = (fs, sc, th) => {
+    return (x, y, z) => fs(x, y, z) >= 0 ? sc(x, y, z) : th(x, y, z);
+}
+
+const abs = expr => {
+    return (x, y, z) => Math.abs(expr(x, y, z));
+}
+
 const operationNames = {
     "+": [add, 2],
     "-": [subtract, 2],
     "*": [multiply, 2],
     "/": [divide, 2],
-    "negate": [negate, 1]
+    "negate": [negate, 1],
+    "iff": [iff, 3],
+    "abs": [abs, 1],
 };
+
+const pi = cnst(Math.PI);
+const e = cnst(Math.E);
+
+const constNames = {
+    "pi": pi,
+    "e": e
+};
+
+
 
 function parse(expression) {
     let stack = [];
@@ -60,9 +80,12 @@ function parse(expression) {
             stack.push(operationNames[token][0](...stack.splice(-operationNames[token][1])));
         } else if (token === 'x' || token === 'y' || token === 'z') {
             stack.push(variable(token));
-        } else {
-            stack.push(cnst(parseInt(token)));
         }
+        else if (token in constNames) {
+            stack.push(constNames[token]);
+        } else {
+             stack.push(cnst(parseInt(token)));
+         }
     }
     return stack.pop();
 }
