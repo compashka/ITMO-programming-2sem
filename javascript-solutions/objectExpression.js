9f51f0ec4842718b6b87f5de030801ca26d583a1
compashka <pavpleshanov@gmail.com>
2022-04-19 06:19:19 +0300

    hw7.2

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -11,42 +11,28 @@ const varNames = {
     "z": 2
 };
 
-let E = new Const(Math.E);
-const diffFunctions = {
-    // :NOTE: В операции
-    "negate": (diffVar, a) => new Negate(a.diff(diffVar)),
-    // :NOTE: Продифференцированные аргументы
-    "+": (diffVar, a, b) => new Add(a.diff(diffVar), b.diff(diffVar)),
-    "-": (diffVar, a, b) => new Subtract(a.diff(diffVar), b.diff(diffVar)),
-    "*": (diffVar, a, b) => new Add(new Multiply(a.diff(diffVar), b), new Multiply(a, b.diff(diffVar))),
-    "/": (diffVar, a, b) => new Divide(
-        new Subtract(new Multiply(a.diff(diffVar), b), new Multiply(a, b.diff(diffVar))), new Multiply(b, b)),
-    // :NOTE: Память
-    "pow": (diffVar, a, b) => new Multiply(new Pow(a, new Subtract(b, new Const(1))), new Add(new Multiply(b,
-        a.diff(diffVar)), new Multiply(new Multiply(a, new Log(E, a)), b.diff(diffVar)))),
-    "log": (diffVar, a, b) => new Divide(new Subtract(new Divide(new Multiply(new Log(E, a), b.diff(diffVar)), b),
-        new Divide(new Multiply(new Log(E, b), a.diff(diffVar)), a)), new Multiply(new Log(E, a), new Log(E, a)))
-};
-
 function Const(value) {
-    this.value = value
+    this.value = value;
 }
 
 function Variable(varName) {
-    this.varName = varName
+    this.varName = varName;
 }
 
-function Operation(...args) {
-    this.args = args
+function Operation(operand, operation, diffFunc, ...args) {
+    this.args = args;
+    this.operand = operand;
+    this.operation = operation;
+    this.diffFunc = diffFunc;
 }
 
-function createExpr(object, evaluate, toString, diff) {
+function createPrototype(object, evaluate, toString, diff) {
     object.prototype.evaluate = evaluate;
     object.prototype.toString = toString;
     object.prototype.diff = diff
 }
 
-createExpr(Const,
+createPrototype(Const,
     function () {
         return +this.value
     },
@@ -56,9 +42,8 @@ createExpr(Const,
     () => constNames[0]
 );
 
-createExpr(Variable,
+createPrototype(Variable,
     function (...args) {
-        // :NOTE: varNames[this.varName]
         return args[varNames[this.varName]]
     },
     function () {
@@ -69,7 +54,7 @@ createExpr(Variable,
     }
 );
 
-createExpr(Operation,
+createPrototype(Operation,
     function (...args) {
         return this.operation(...this.args.map(f => f.evaluate(...args)))
     },
@@ -81,26 +66,36 @@ createExpr(Operation,
     }
 );
 
-function makeOperation(operand, operation) {
+function makeOperation(operand, operation, diffFunc) {
     const result = function (...args) {
-        Operation.call(this, ...args)
+        Operation.call(this, operand, operation, diffFunc, ...args)
     };
     result.prototype = Object.create(Operation.prototype);
-    result.prototype.operand = operand;
-    result.prototype.operation = operation;
-    result.prototype.diffFunc = diffFunctions[operand];
-    // :NOTE: .constructor
     result.opLength = operation.length;
+
     return result
 }
 
-const Negate = makeOperation("negate", a => -a);
-const Add = makeOperation("+", (a, b) => a + b);
-const Subtract = makeOperation("-", (a, b) => a - b);
-const Multiply = makeOperation("*", (a, b) => a * b);
-const Divide = makeOperation("/", (a, b) => a / b);
-const Pow = makeOperation("pow", (a, b) => Math.pow(a, b));
-const Log = makeOperation("log", (a, b) => Math.log(Math.abs(b)) / Math.log(Math.abs(a)));
+let E = new Const(Math.E);
+let One = new Const(1);
+
+const Negate = makeOperation("negate", a => -a,
+    (diffVar, a) => new Negate(a.diff(diffVar)));
+const Add = makeOperation("+", (a, b) => a + b,
+    (diffVar, a, b) => new Add(a.diff(diffVar), b.diff(diffVar)));
+const Subtract = makeOperation("-", (a, b) => a - b,
+    (diffVar, a, b) => new Subtract(a.diff(diffVar), b.diff(diffVar)));
+const Multiply = makeOperation("*", (a, b) => a * b,
+    (diffVar, a, b) => new Add(new Multiply(a.diff(diffVar), b), new Multiply(a, b.diff(diffVar))));
+const Divide = makeOperation("/", (a, b) => a / b,
+    (diffVar, a, b) => new Divide(
+        new Subtract(new Multiply(a.diff(diffVar), b), new Multiply(a, b.diff(diffVar))), new Multiply(b, b)));
+const Pow = makeOperation("pow", (a, b) => Math.pow(a, b),
+    (diffVar, a, b) => new Multiply(new Pow(a, new Subtract(b, One)),
+        new Add(new Multiply(b, a.diff(diffVar)), new Multiply(new Multiply(a, new Log(E, a)), b.diff(diffVar)))));
+const Log = makeOperation("log", (a, b) => Math.log(Math.abs(b)) / Math.log(Math.abs(a)),
+    (diffVar, a, b) => new Divide(new Subtract(new Divide(new Multiply(new Log(E, a), b.diff(diffVar)), b),
+        new Divide(new Multiply(new Log(E, b), a.diff(diffVar)), a)), new Multiply(new Log(E, a), new Log(E, a))));
 const Min3 = makeOperation("min3", (a, b, c) => Math.min(a, b, c));
 const Max5 = makeOperation("max5", (a, b, c, d, e) => Math.max(a, b, c, d, e));
 
